986b7e78d82192752a81ea428cc2443e2249006b
Jesse Nelson <spheromak@gmail.com>
2015-04-16 10:54:40 -0700

    Fixup URL assembly, add simple test

--- openstack/compute/v2/images/requests.go
+++ openstack/compute/v2/images/requests.go
@@ -66,6 +66,6 @@ func Get(client *gophercloud.ServiceClient, id string) GetResult {
 
 func Delete(client *gophercloud.ServiceClient, id string) DeleteResult {
 	var result DeleteResult
-	_, result.Err = client.Delete(getURL(client, id), nil)
+	_, result.Err = client.Delete(deleteURL(client, id), nil)
 	return result
 }
